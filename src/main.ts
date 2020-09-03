902f5d7900bb3365f4b1c7efb0b8f02db96fe14e
sitong <sitongpeng@gmail.com>
2020-09-02 17:25:04 -0700

    ip

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as Webhooks from '@octokit/webhooks'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (github.context.eventName === 'pull_request') {
+      const { pull_request } = github.context.payload as Webhooks.EventPayloads.WebhookPayloadPullRequest
+      const patchNoteRegex = /<!-- Patch Note Start -->(.+):\s?(.+)<!-- Patch Note End -->/
+      const [ _0, patchNoteType, patchNote ]= patchNoteRegex.exec(pull_request.body) || []
+      if (patchNoteType && patchNote) {
+        console.log('Found patch note type:', patchNoteType)
+        console.log('Found patch note:', patchNote)
+        core.setOutput("type", patchNoteType)
+        core.setOutput("patchNote", patchNote)
+      }
+   }
   } catch (error) {
     core.setFailed(error.message)
   }
