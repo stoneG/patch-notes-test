853f3d423125f5584ebaede926f7e775d8810d78
stoneG <sitongpeng@gmail.com>
2020-09-03 18:17:37 -0700

    refactor: patchNote -> note

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,16 @@ async function run(): Promise<void> {
       const { pull_request } = github.context.payload as Webhooks.EventPayloads.WebhookPayloadPullRequest
       console.log('pull_request', JSON.stringify(pull_request))
       const patchNoteRegex = /<!-- Patch Note Start -->\r*\n*([a-z]+)\(?([a-z]+)\)?:\s?(.+)\r*\n*<!-- Patch Note End -->|<!-- Patch Note Start -->\r*\n*n\/ar*\n*<!-- Patch Note End -->/
-      const [ match, type, context, patchNote ]= patchNoteRegex.exec(pull_request.body) || []
+      const [ match, type, context, note ]= patchNoteRegex.exec(pull_request.body) || []
       if (!match) {
         throw Error('Could not find patch note in the pull request body. Please use the format `n/a` or `{type}: {notes}`.')
       }
-      if (match && (!type || !patchNote)) {
+      if (match && (!type || !note)) {
         console.log('Found n/a, so no patch note is necessary.')
       }
       core.setOutput("type", type)
       core.setOutput("context", context)
-      core.setOutput("patchNote", patchNote)
+      core.setOutput("note", note)
     } else {
       throw Error('This action must be run in a pull request event.')
     }
